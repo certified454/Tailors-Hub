237a9e094cd989dbac28536535ad114861bbefa8
certified454 <fj689669@gmail.com>
2025-12-24 19:06:28 -0800

    user module added

--- src/modules/user.js
+++ src/modules/user.js
@@ -1,17 +1,17 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// const userSchema = new mongoose.Schema({
-//     name: {
-//         type: String,
-//         required: true
-//     },
-//     contact: {
-//         type: String,
-//         required: true,
-//         unique: true
-//     }
-// }, { timestamps: true });
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    contact: {
+        type: String,
+        required: true,
+        unique: true
+    }
+}, { timestamps: true });
 
-// const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
-// export default User;
+export default User;
