aff68143bc3f97f970363b8169b5d794738e87ac
certified454 <fj689669@gmail.com>
2025-12-24 19:04:43 -0800

    created src and sub directories and files needed for the project

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,27 @@
+// import express from "express";
+// import User from "../modules/user.js";
+
+// const router = express.Router();
+
+// router.post("/users", async (req, res) => {
+//     const { name, contact } = req.body;
+
+//     try {
+//         if (!name || !contact) {
+//             return res.status(400).json({ message: "Name and contact are required." });
+//         };
+//         if (contact.length < 10) {
+//             return res.status(400).json({ message: "Contact number must be at least 10 digits long." });
+//         }
+//         const newUser = new User({
+//             name,
+//             contact 
+//         });
+//         await newUser.save();
+//         res.status(201).json(newUser);
+//     } catch (error) {
+//         res.status(500).json({ message: error.message });
+//     }
+// });
+
+// export default router;
